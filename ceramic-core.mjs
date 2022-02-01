c7b4a28c53c3df09c3229682ad35076feea3ca90
hiaux0 <hieudn5293@yahoo.de>
2022-02-01 01:15:11 +0100

    fix(): some wrong vars

--- ceramic-core.mjs
+++ ceramic-core.mjs
@@ -1,5 +1,5 @@
 import Ceramic from '@ceramicnetwork/core'
-import IPFS from 'ipfs-core'
+import * as IPFS from 'ipfs-core'
 import dagJose from 'dag-jose'
 import { convert } from 'blockcodec-to-ipld-format'
 import KeyDidResolver from 'key-did-resolver'
@@ -9,7 +9,8 @@ import { DID } from 'dids'
 
 (async () => {
   const dagJoseFormat = convert(dagJose)
-  const ipfs = await Ipfs.create({ ipld: { formats: [dagJoseFormat] } })
+  const ipfs = await IPFS.create({ ipld: { formats: [dagJoseFormat] } })
+  const ceramic = await Ceramic.create(ipfs, {})
   const resolver = { ...KeyDidResolver.getResolver(),
                      ...ThreeIdResolver.getResolver(ceramic) }
   const did = new DID({ resolver })
